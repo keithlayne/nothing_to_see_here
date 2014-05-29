f907c69dc08cb8be0521a18da234eef9eadb362e
Keith Layne <klayne@trupointpartners.com>
2014-05-29 07:47:11 -0400

    A little better.  Rule should take two functions.

---
+++ third_try_is_a_charm.hs
@@ -0,0 +1,26 @@
+-- Still not great, but getting better.
+-- Don't judge me.  You don't know my life.
+
+import Control.Monad (mapM_)
+
+data Rule a b = Rule {
+    test :: (a -> Bool),
+    result :: (a -> b)
+  }
+
+modp :: (Integral a) => a -> a -> Bool
+modp n = (== 0) . (`mod` n)
+
+fizz = Rule (modp 3) (const "fizz")
+buzz = Rule (modp 5) (const "buzz")
+fizzbuzz = Rule (modp 15) (const "fizzbuzz")
+number = Rule (const True) (show :: Integer -> String)
+
+tryRule :: Rule a b -> a -> Either b a
+tryRule rule input | test rule $ input = Left $ result rule $ input
+                   | otherwise = Right input
+
+runFizzBuzz n = return n >>= tryRule fizzbuzz >>= tryRule buzz >>= tryRule fizz >>= tryRule number
+
+main = mapM_ (putStrLn . run) [1..30]
+  where run = either show undefined . runFizzBuzz
