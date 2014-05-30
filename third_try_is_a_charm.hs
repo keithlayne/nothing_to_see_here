e2af9548df94a7070f25fd0529357c3e368067c4
Keith Layne <klayne@trupointpartners.com>
2014-05-29 21:25:57 -0400

    That's just a little nicer.

--- third_try_is_a_charm.hs
+++ third_try_is_a_charm.hs
@@ -5,8 +5,8 @@ import Control.Monad (mapM_)
 import Control.Applicative ((<$>))
 
 data Rule a b = Rule {
-    test :: (a -> Bool),
-    result :: (a -> b)
+    test :: a -> Bool,
+    result :: a -> b
   }
 
 modp :: (Integral a) => a -> a -> Bool
@@ -18,9 +18,9 @@ fizzbuzz = Rule (modp 15) (const "fizzbuzz")
 number = Rule (const True) (show :: Integer -> String)
 
 tryRule :: Rule a b -> a -> Either b a
-tryRule rule input | test rule $ input = Left $ result rule $ input
+tryRule rule input | test rule input = Left $ result rule input
                    | otherwise = Right input
 
-runFizzBuzz n = return n >>= tryRule fizzbuzz >>= tryRule buzz >>= tryRule fizz >>= tryRule number
+runFizzBuzz n = tryRule fizzbuzz n >>= tryRule buzz >>= tryRule fizz >>= tryRule number
 
 main = mapM_ (either putStrLn undefined) $ runFizzBuzz <$> [1..30]
